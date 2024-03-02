0fb168d5202508dfb25926046b8f82931f1e259b
doniwyk <doniwyk@gmail.com>
2024-03-02 12:18:34 +0700

    add routes for each views
    adding routes for home page, category page, user page, and sales pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'home']);
+Route::prefix('category')->group(function () {
+    Route::get('/{category}', [CategoryController::class, 'show'])->name('categories.show');
 });
+Route::get('user/{id}/name/{name}', [UserController::class, 'user']);
+Route::get('/sales', [SalesController::class, 'sales']);
